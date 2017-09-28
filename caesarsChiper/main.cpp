6975bd9b111179b38bd5964ad1dcbf9cf662c9dd
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2017-09-28 15:23:13 +0300

    Update main.cpp

--- caesarsChiper/main.cpp
+++ caesarsChiper/main.cpp
@@ -7,9 +7,13 @@ int main()
 {
   string input;
   int count = 0, length;
+  int shift;
 
-  cout << "Enter your phrase: \n";
+  cout << "Enter a phrase: \n";
   getline(cin, input);
+
+  cout << "\nHow many times do you want to shift the letter(s) in the message? ";
+  cin >> shift;
 
   length = input.length();
 
@@ -18,7 +22,7 @@ int main()
     if(isalpha(input[count]))
     {
       input[count] = tolower(input[count]);
-      for (int i = 0; i < 3; i++)
+      for (int i = 0; i < shift; i++)
       {
         if (input[count] == 'z')
           input[count] = 'a';
@@ -28,6 +32,6 @@ int main()
     }
   }
 
-  cout << input << endl;
+  cout << "\nResult: \n" <<input << endl;
 
 }

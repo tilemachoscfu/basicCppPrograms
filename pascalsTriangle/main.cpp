a5995e9b153a18590322f6adf5ce6df7673e6311
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2017-09-29 15:39:34 +0300

    Add files via upload

---
+++ pascalsTriangle/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+    int value,rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << endl;
+
+    for(i = 0;i < rows; i++)
+    {
+        value = 1;
+        for(j = 0; j < rows-1-i; j++)
+        {
+            cout << " ";
+        }
+        for(k = 0; k <= i; k++)
+        {
+            cout << " " << value;
+            value = value*(i-k)/(k+1); // Pascals triangle equation in iteration form.
+        }
+    cout << endl << endl;
+    }
+    return 0;
+}

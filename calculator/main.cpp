4300fc84483c5e2672ec2d19d1bd1c3c406ad1b8
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2017-10-29 14:19:48 +0200

    Add files via upload

---
+++ calculator/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+    cout << "Enter operator (+,-,*,/): ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << num1 + num2;
+            break;
+
+        case '-':
+            cout << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 * num2;
+            break;
+
+        case '/':
+            cout << num1 / num2;
+            break;
+
+        default:
+            cout << "Operator is not correct";
+            break;
+    }
+    return 0;
+}
